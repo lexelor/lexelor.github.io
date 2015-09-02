c3a16e284b25e6a2ed30bf228c3991be52eadddc
lexelor <rozanovz13@gmail.com>
2015-09-02 20:49:56 +0300

    new pro

--- js/controllers.js
+++ js/controllers.js
@@ -5,6 +5,7 @@ portfolio.controller('worksCtrl', function ($scope) {
 
     $http.get('../projects/works.json').success(function(data) {
         $scope.works = data;
-      });
+        console.log("hello");
+    });
     
 });
