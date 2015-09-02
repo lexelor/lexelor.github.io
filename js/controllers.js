e6d23fd4436797f76451618800c3cf7288846773
lexelor <rozanovz13@gmail.com>
2015-09-02 20:42:44 +0300

    new pro

--- js/controllers.js
+++ js/controllers.js
@@ -2,60 +2,9 @@
 
 /* Controllers */
 portfolio.controller('worksCtrl', function ($scope) {
-  $scope.works = [
-    {
-        img:'img/magmagazine-preview.jpg',
-        name:'MagMagazine',
-        link: '../magmagazine.html'
-    },
+
+    $http.get('projects/works.json').success(function(data) {
+        $scope.works = data;
+      });
     
-    {
-        img:'img/letusmake-preview.jpg',
-        name:'Let Us Make',
-        link: '../letusmake.html'
-    },
-    
-    {
-        img:'img/GRAphica-preview.jpg',
-        name:'GRAphica',
-        link: '../photographicas.html'
-    },
-    
-    {
-        img:'img/flat-corporate-preview.jpg',
-        name:'Flat Corporate',
-        link: '../projects/FlatCorporate/index.htm'
-    },
-    
-    {
-        img:'img/Math-app-preview.jpg',
-        name:'Math App',
-        link: '../mathapp.html'
-    },
-    
-    {
-        img:'img/reen-preview.jpg',
-        name:'Reen',
-        link: '../reen.html'
-    },
-    
-    {
-        img:'img/lexelorLanding.jpg',
-        name:'Lexelor landing page',
-        link: 'http://lexelor.com/'
-    },
-    
-    {
-        img:'img/lexelor.jpg',
-        name:'Lexelor home',
-        link: '../projects/Lexelor/index.html'
-    },
-    	
-    {
-        img:'img/oneluke.jpg',
-        name:'Oneluke',
-        link: '../oneluke.html'
-    },
-        
-	];
 });
